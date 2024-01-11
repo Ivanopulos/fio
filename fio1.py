70c1bf13d1d4ed2f5d9e561a73ea3a2b7a7f08a8
Ivanopulos <Pochta.rus.lc@gmail.com>
2024-01-11 16:25:26 +0300

    базфин

--- fio1.py
+++ fio1.py
@@ -2,15 +2,57 @@ import pandas as pd
 
 df_meetingbase = pd.read_excel('combined_data_with_votes.xlsx', dtype=str)
 df_gender = pd.read_excel('gndr.xlsx', dtype=str)
-# Создаем столбец с вторым словом из "ФИО правообладателя"
-df_meetingbase['second_name'] = df_meetingbase['ФИО правообладателя'].apply(
-    lambda x: x.split()[1] if isinstance(x, str) and len(x.split()) > 1 else None
+def extract_word(name, index):
+    if isinstance(name, str) and len(name.split()) > index:
+        return name.split()[index]
+    return None
+df_meetingbase['second_name'] = df_meetingbase['ФИО правообладателя'].apply(lambda x: extract_word(x, 1))# Извлекаем второе слово
+df_meetingbase = df_meetingbase.merge(df_gender, left_on='second_name', right_on='name', how='left', suffixes=('', '_y'))# Объединяем df_meetingbase и df_gender по второму слову
+mask = df_meetingbase['gender'].isnull() & df_meetingbase['ФИО правообладателя'].apply(lambda x: isinstance(x, str) and len(x.split()) == 4)# Для записей, где gender не найден и есть четыре слова в ФИО, пытаемся найти по третьему слову
+df_meetingbase.loc[mask, 'third_name'] = df_meetingbase['ФИО правообладателя'].apply(lambda x: extract_word(x, 2))
+merged_df = df_meetingbase[mask].merge(df_gender, left_on='third_name', right_on='name', how='left', suffixes=('', '_y'))
+df_meetingbase.loc[mask, 'gender'] = merged_df['gender_y']
+df_meetingbase.drop(['second_name', 'third_name'], axis=1, inplace=True)# Убираем временные столбцы
+def gender_from_lastname(name):
+    if name.endswith('на'):
+        return 'f'
+    elif name.endswith('ич'):
+        return 'm'
+    return None
+df_meetingbase['gender'] = df_meetingbase.apply(
+    lambda row: gender_from_lastname(row['ФИО правообладателя']) if row['gender'] not in ['f', 'm'] else row['gender'],
+    axis=1
 )
-# Объединяем df_meetingbase и df_gender
-df_meetingbase = df_meetingbase.merge(df_gender, left_on='second_name', right_on='name', how='left')
-# Переименовываем столбец gender из df_gender для ясности
-df_meetingbase.rename(columns={'gender': 'gender_matched'}, inplace=True)
-# Удаляем временный столбец second_name, если он больше не нужен
-df_meetingbase.drop('second_name', axis=1, inplace=True)
+def gender_from_firstname(name):
+    if name.endswith('ов'):
+        return 'm'
+    elif name.endswith('ва'):
+        return 'f'
+    return None
+df_meetingbase.loc[df_meetingbase['gender'].isin(['m', 'f']) == False, 'gender'] = df_meetingbase[df_meetingbase['gender'].isin(['m', 'f']) == False]['ФИО правообладателя'].apply(lambda x: x.split()[0] if isinstance(x, str) else None).apply(gender_from_firstname)
+df_meetingbase.loc[df_meetingbase['gender'].isnull(), 'gender'] = 'u'
 
-print(df_meetingbase)
+def modify_name_based_on_gender(name, gender):
+    if not isinstance(name, str):
+        return name
+
+    words = name.split()
+    if gender == 'f':
+        # Для женского пола: изменяем окончания слов
+        words = [w[:-1] + 'у' if w.endswith('а') else w for w in words]
+        words = [w[:-1] + 'ю' if w.endswith('я') and words.index(w) in [1, 2] else w for w in words]
+    elif gender == 'm':
+        # Для мужского пола: изменяем окончания слов
+        if words[0].endswith('в'):
+            words[0] = words[0][:-1] + 'ва'
+        if len(words) > 1 and words[1][-1] in 'бвгджзклмнпрстфхцчшщ':
+            words[1] = words[1] + 'а'
+        if words[-1].endswith('ич'):
+            words[-1] = words[-1] + 'а'
+
+    return ' '.join(words)
+
+df_meetingbase['кого'] = df_meetingbase.apply(                     # Создаем столбец "кого" с изменениями
+    lambda row: modify_name_based_on_gender(row['ФИО правообладателя'], row['gender']), axis=1)
+df_meetingbase.to_excel('1.xlsx', index=False)
+
